9cd75777c93ce80f31d62319165688e21258b11c
NikitaLoginov <loginoff.n@gmail.com>
2021-02-01 13:32:50 +0200

    inital commit. showing off inertia sumo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject gameOverScreen;
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    private void Update()
+    {
+        if (player.transform.position.y < -20)
+        {
+            GameOver();
+        }
+    }
+    void GameOver()
+    {
+        Time.timeScale = 0f;
+        gameOverScreen.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
